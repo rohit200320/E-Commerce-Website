0a4a64532e92d893e4ea40abd2c7127784e87041
rohit200320 <rohitphuge.2003@gmail.com>
2024-07-24 22:31:32 +0530

    fixed navbar-button for medium & small screen devices

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const close = document.getElementById('close');
 
 if(bar)
 {
-    bar.addEventListener('click',()=>{
+    bar.addEventListener('click', () => {
         nav.classList.add('active');
     })
 }
 
 if(close)
 {
-    close.addEventListener('click',()=>{
+    close.addEventListener('click', () => {
         nav.classList.remove('active');
     })
 }
